213579865ec93348ccbaaf5b04a024a8b96d0dd3
Marius <marius.horghidan@yahoo.com>
2024-03-29 14:49:11 +0200

    create base-logo component

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,4 +1,5 @@
 import { type ReactNode } from "react";
+import { BaseLogo } from "@/components";
 
 type Props = {
   children: ReactNode;
@@ -7,9 +8,18 @@ type Props = {
 export function MainLayout({ children }: Props) {
   return (
     <>
-      <header></header>
+      <header className="bg-slate-300 py-6">
+        <div className="container-lg mx-auto">
+          <BaseLogo />
+          <Navbar />
+        </div>
+      </header>
       <main>{children}</main>
       <footer></footer>
     </>
   );
 }
+
+export const Navbar = (): React.JSX.Element => {
+  return <nav></nav>;
+};
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,12 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 import { WEBSITE_TITLE } from "./constants";
 
 export function getPageTitle(page: string) {
   return `${page} | ${WEBSITE_TITLE}`;
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
