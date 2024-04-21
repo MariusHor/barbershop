0925817f4387ead9c3afc6fe85e8c9c46caae83d
Marius <marius.horghidan@yahoo.com>
2024-04-21 13:23:18 +0300

    add sanity schemas validation rules

--- src/schemas/locations.ts
+++ src/schemas/locations.ts
@@ -1,3 +1,5 @@
+import { type Rule } from "@sanity/types";
+
 const locatii = {
   name: "location",
   type: "document",
@@ -7,31 +9,37 @@ const locatii = {
       name: "name",
       type: "string",
       title: "Nume",
+      validation: (rule: Rule) => rule.required().min(4).max(50),
     },
     {
       name: "street",
       type: "string",
       title: "Strada",
+      validation: (rule: Rule) => rule.required().min(10).max(50),
     },
     {
       name: "city",
       type: "string",
       title: "Oras",
+      validation: (rule: Rule) => rule.required().min(3).max(30),
     },
     {
       name: "zip",
-      type: "string",
+      type: "number",
       title: "Cod Postal",
+      validation: (rule: Rule) => rule.required().integer(),
     },
     {
       name: "phone",
       type: "string",
       title: "Telefon",
+      validation: (rule: Rule) => rule.required().min(8).max(30),
     },
     {
       name: "email",
       type: "string",
       title: "Email",
+      validation: (rule: Rule) => rule.required().email(),
     },
     {
       name: "socialPlatforms",
@@ -47,6 +55,7 @@ const locatii = {
               title: "Nume",
               name: "name",
               type: "string",
+              validation: (rule: Rule) => rule.required(),
               options: {
                 list: [
                   { title: "Facebook", value: "facebook" },
@@ -73,6 +82,7 @@ const locatii = {
           name: "timetable",
           type: "string",
           title: "Orar",
+          validation: (rule: Rule) => rule.required().min(10).max(50),
         },
       ],
     },
