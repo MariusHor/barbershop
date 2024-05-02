aad499e81a6e418e25d71d7e223fd4125c3377ac
Marius <marius.horghidan1@gmail.com>
2024-05-02 08:47:02 +0300

    [SB-2]: create getHeroImage procedure

--- src/server/api/routers/content.ts
+++ src/server/api/routers/content.ts
@@ -1,6 +1,11 @@
 import { type Url } from "next/dist/shared/lib/router/router";
 
-import type { SiteLogo, ShopLocation, SiteSettings } from "sanity.types";
+import type {
+  SiteLogo,
+  ShopLocation,
+  SiteSettings,
+  HeroImage,
+} from "sanity.types";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 import { TRPCError } from "@trpc/server";
 import { SANITY_DOC_TYPES } from "@/utils/constants";
@@ -14,30 +19,41 @@ function throwSanityErrorMessage({ dataType }: { dataType: string }) {
 
 export const sanityContentRouter = createTRPCRouter({
   getSiteSettings: publicProcedure.query(async ({ ctx }) => {
-    const query = `*[_type == "${SANITY_DOC_TYPES.siteSettings}"]`;
-
+    const dataType = SANITY_DOC_TYPES.siteSettings;
+    const query = `*[_type == "${dataType}"]`;
     const data =
       await ctx.sanityClient.fetch<(SiteSettings & { scheduleLink: Url })[]>(
         query,
       );
 
-    if (!data[0]) throwSanityErrorMessage({ dataType: "siteSettings" });
+    if (!data[0]) throwSanityErrorMessage({ dataType });
 
     return data[0];
   }),
   getSiteLogo: publicProcedure.query(async ({ ctx }) => {
-    const query = `*[_type == "${SANITY_DOC_TYPES.siteLogo}"]`;
+    const dataType = SANITY_DOC_TYPES.siteLogo;
+    const query = `*[_type == "${dataType}"]`;
     const data = await ctx.sanityClient.fetch<SiteLogo[]>(query);
 
-    if (!data[0]) throwSanityErrorMessage({ dataType: "siteLogo" });
+    if (!data[0]) throwSanityErrorMessage({ dataType });
 
     return data[0];
   }),
   getShopLocation: publicProcedure.query(async ({ ctx }) => {
-    const query = `*[_type == "${SANITY_DOC_TYPES.shopLocation}"]`;
+    const dataType = SANITY_DOC_TYPES.shopLocation;
+    const query = `*[_type == "${dataType}"]`;
     const data = await ctx.sanityClient.fetch<ShopLocation[]>(query);
 
-    if (!data[0]) throwSanityErrorMessage({ dataType: "shopLocation" });
+    if (!data[0]) throwSanityErrorMessage({ dataType });
+
+    return data[0];
+  }),
+  getHeroImage: publicProcedure.query(async ({ ctx }) => {
+    const dataType = SANITY_DOC_TYPES.heroImage;
+    const query = `*[_type == "${dataType}"]`;
+    const data = await ctx.sanityClient.fetch<HeroImage[]>(query);
+
+    if (!data[0]) throwSanityErrorMessage({ dataType });
 
     return data[0];
   }),
