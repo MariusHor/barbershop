27a6ccefa772bada068216014a051816b3a87e2c
Marius <marius.horghidan1@gmail.com>
2024-05-03 09:50:54 +0300

    [SB-2]: add base page content building schema

--- next.config.js
+++ next.config.js
@@ -17,6 +17,15 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default config;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,15 @@ import { getPageTitle } from "@/utils/helpers";
 import { getSSGHelper } from "@/utils/getSSGHelper";
 import { PAGE_ROUTES } from "@/utils/constants";
 import { api } from "@/utils/api";
+import Image from "next/image";
+import { urlFor } from "@/lib/sanity/client";
+import { ScheduleButton } from "@/components";
 
 export const getServerSideProps = async () => {
   const ssg = getSSGHelper();
 
   await Promise.all([
+    ssg.content.getHeroImage.prefetch(),
     ssg.content.getSiteSettings.prefetch(),
     ssg.content.getSiteLogo.prefetch(),
     ssg.content.getShopLocation.prefetch(),
@@ -27,6 +31,7 @@ const Page: NextPageWithLayout<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = () => {
   const { data: siteSettings } = api.content.getSiteSettings.useQuery();
+  const { data: heroImage } = api.content.getHeroImage.useQuery();
   const pageName = PAGE_ROUTES.home.name;
 
   return (
@@ -35,8 +40,28 @@ const Page: NextPageWithLayout<
         <title>{getPageTitle(pageName, siteSettings?.title)}</title>
         <meta name="description" content={siteSettings?.description} />
       </Head>
-      <div className="hero grid w-full place-items-center">
-        <h1>Sergiu Barbershop</h1>
+      <div className="hero relative flex w-full flex-col items-center justify-center bg-black">
+        {heroImage?.imgUrl ? (
+          <Image
+            src={urlFor(heroImage?.imgUrl).url()}
+            alt={"hero image"}
+            width={heroImage?.width}
+            height={heroImage?.height}
+            className="absolute left-0 top-0 h-full w-full object-cover opacity-40"
+            priority
+          />
+        ) : null}
+
+        <div className="flex max-w-3xl flex-col items-center gap-4">
+          <h1 className="z-10 text-center text-8xl font-bold text-white opacity-100">
+            Barbershop-ul tau in Iasi
+          </h1>
+          <ScheduleButton
+            className="z-10"
+            text="Fa o programare"
+            variant={"secondary"}
+          />
+        </div>
       </div>
     </>
   );
